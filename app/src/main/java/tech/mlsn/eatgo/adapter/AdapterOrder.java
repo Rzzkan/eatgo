b5d95643fb6f0801bc4eac797487a0854b1e475b
Aditya Nur Riskan Nugroho <xcyberd@gmail.com>
2021-07-31 07:29:17 +0700

    fixing bug upload photo

--- app/src/main/java/tech/mlsn/eatgo/adapter/AdapterOrder.java
+++ app/src/main/java/tech/mlsn/eatgo/adapter/AdapterOrder.java
@@ -1,140 +1,144 @@
-//package tech.mlsn.eatgo.adapter;
-//
-//import android.content.Context;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.Filter;
-//import android.widget.Filterable;
-//import android.widget.LinearLayout;
-//import android.widget.TextView;
-//
-//import androidx.cardview.widget.CardView;
-//import androidx.recyclerview.widget.RecyclerView;
-//
-//import com.mikhaellopez.circularimageview.CircularImageView;
-//import java.util.ArrayList;
-//
-//public class AdapterOrder extends RecyclerView.Adapter<RecyclerView.ViewHolder> implements Filterable {
-//
-//    private ArrayList<UserOrderDataResponse> items = new ArrayList<>();
-//    private ArrayList<UserOrderDataResponse> itemsFiltered = new ArrayList<>();
-//
-//    private Context ctx;
-//    private OnItemClickListener mOnItemClickListener;
-//
-//
-//    public interface OnItemClickListener {
-//        void onItemClick(View view, UserOrderDataResponse obj, int position);
-//    }
-//
-//    public void setOnItemClickListener(final OnItemClickListener mItemClickListener) {
-//        this.mOnItemClickListener = mItemClickListener;
-//    }
-//
-//    public AdapterOrder(Context context, ArrayList<UserOrderDataResponse> items) {
-//        this.items = items;
-//        this.itemsFiltered = items;
-//        ctx = context;
-//    }
-//
-//    public class OriginalViewHolder extends RecyclerView.ViewHolder {
-//        CircularImageView imgProfile;
-//        TextView tvName, tvDate, tvInvoice;
-//        CardView lytCard;
-//        LinearLayout lytParent;
-//        View lyt_Ripple;
-//
-//        public OriginalViewHolder(View v) {
-//            super(v);
-//            imgProfile = v.findViewById(R.id.imgProfile);
-//            tvName = v.findViewById(R.id.tvName);
-//            tvDate = v.findViewById(R.id.tvDate);
-//            tvInvoice = v.findViewById(R.id.tvInvoice);
-//            lytCard = v.findViewById(R.id.lytCard);
-//            lytParent = v.findViewById(R.id.lyt_parent);
-//            lyt_Ripple = v.findViewById(R.id.lyt_ripple);
-//        }
-//    }
-//
-//    @Override
-//    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-//        RecyclerView.ViewHolder vh;
-//        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_order, parent, false);
-//        vh = new OriginalViewHolder(v);
-//        return vh;
-//    }
-//
-//    // Replace the contents of a view (invoked by the layout manager)
-//    @Override
-//    public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
-//        if (holder instanceof OriginalViewHolder) {
-//            OriginalViewHolder view = (OriginalViewHolder) holder;
-//            UserOrderDataResponse p = itemsFiltered.get(position);
-//            view.tvInvoice.setText("Rp." + p.getTotalPrice());
-//            view.tvDate.setText(Tools.dateParser(p.getCreatedAt()));
-//            view.tvName.setText(p.getPemesanSembako());
-//            view.lyt_Ripple.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    if (mOnItemClickListener != null) {
-//                        mOnItemClickListener.onItemClick(v, itemsFiltered.get(position), position);
-//                    }
-//                }
-//            });
-//        }
-//    }
-//
-//    @Override
-//    public int getItemCount() {
-//        return itemsFiltered.size();
-//    }
-//
-//    public void insertItem(UserOrderDataResponse item) {
-//        this.items.add(item);
-//        notifyItemInserted(getItemCount());
-//        if (getItemCount() > 1) notifyItemChanged(getItemCount() - 2);
-//    }
-//
-//    @Override
-//    public Filter getFilter() {
-//        return new Filter() {
-//            @Override
-//            protected FilterResults performFiltering(CharSequence charSequence) {
-//                String charString = charSequence.toString();
-//                if (charString.isEmpty()) {
-//                    itemsFiltered = items;
-//                }else if (Integer.parseInt(charString)==1){
-//                    ArrayList<UserOrderDataResponse> filteredList = new ArrayList<>();
-//                    for (UserOrderDataResponse data : items) {
-//                        String status = data.getStatus();
-//                        if(status.equalsIgnoreCase("")){
-//                            filteredList.add(data);
-//                        }
-//                    }
-//                    itemsFiltered = filteredList;
-//                }else if (Integer.parseInt(charString)==0)  {
-//                    ArrayList<UserOrderDataResponse> filteredList = new ArrayList<>();
-//                    for (UserOrderDataResponse data : items) {
-//                        String  status = data.getStatus();
-//                        if(status.equalsIgnoreCase("")){
-//                            filteredList.add(data);
-//                        }
-//                    }
-//                    itemsFiltered = filteredList;
-//                }
-//                FilterResults filterResults = new FilterResults();
-//                filterResults.values = itemsFiltered;
-//                return filterResults;
-//            }
-//
-//            @Override
-//            protected void publishResults(CharSequence constraint, FilterResults results) {
-//                itemsFiltered = (ArrayList<UserOrderDataResponse>) results.values;
-//                notifyDataSetChanged();
-//            }
-//        };
-//
-//    }
-//
-//}
+package tech.mlsn.eatgo.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mikhaellopez.circularimageview.CircularImageView;
+import java.util.ArrayList;
+
+import tech.mlsn.eatgo.R;
+import tech.mlsn.eatgo.response.OrdersDataResponse;
+import tech.mlsn.eatgo.tools.Tools;
+
+public class AdapterOrder extends RecyclerView.Adapter<RecyclerView.ViewHolder> implements Filterable {
+
+    private ArrayList<OrdersDataResponse> items = new ArrayList<>();
+    private ArrayList<OrdersDataResponse> itemsFiltered = new ArrayList<>();
+
+    private Context ctx;
+    private OnItemClickListener mOnItemClickListener;
+
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, OrdersDataResponse obj, int position);
+    }
+
+    public void setOnItemClickListener(final OnItemClickListener mItemClickListener) {
+        this.mOnItemClickListener = mItemClickListener;
+    }
+
+    public AdapterOrder(Context context, ArrayList<OrdersDataResponse> items) {
+        this.items = items;
+        this.itemsFiltered = items;
+        ctx = context;
+    }
+
+    public class OriginalViewHolder extends RecyclerView.ViewHolder {
+        CircularImageView imgProfile;
+        TextView tvName, tvDate, tvInvoice;
+        CardView lytCard;
+        LinearLayout lytParent;
+        View lyt_Ripple;
+
+        public OriginalViewHolder(View v) {
+            super(v);
+            imgProfile = v.findViewById(R.id.imgProfile);
+            tvName = v.findViewById(R.id.tvName);
+            tvDate = v.findViewById(R.id.tvDate);
+            tvInvoice = v.findViewById(R.id.tvInvoice);
+            lytCard = v.findViewById(R.id.lytCard);
+            lytParent = v.findViewById(R.id.lyt_parent);
+            lyt_Ripple = v.findViewById(R.id.lyt_ripple);
+        }
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        RecyclerView.ViewHolder vh;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_order, parent, false);
+        vh = new OriginalViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
+        if (holder instanceof OriginalViewHolder) {
+            OriginalViewHolder view = (OriginalViewHolder) holder;
+            OrdersDataResponse p = itemsFiltered.get(position);
+            view.tvInvoice.setText("Rp." + p.getTotal());
+            view.tvDate.setText(Tools.dateParserChat(p.getDate()));
+            view.tvName.setText(p.getIdUser());
+            view.lyt_Ripple.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mOnItemClickListener != null) {
+                        mOnItemClickListener.onItemClick(v, itemsFiltered.get(position), position);
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemsFiltered.size();
+    }
+
+    public void insertItem(OrdersDataResponse item) {
+        this.items.add(item);
+        notifyItemInserted(getItemCount());
+        if (getItemCount() > 1) notifyItemChanged(getItemCount() - 2);
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String charString = charSequence.toString();
+                if (charString.isEmpty()) {
+                    itemsFiltered = items;
+                }else if (Integer.parseInt(charString)==1){
+                    ArrayList<OrdersDataResponse> filteredList = new ArrayList<>();
+                    for (OrdersDataResponse data : items) {
+                        String status = data.getStatus();
+                        if(status.equalsIgnoreCase("")){
+                            filteredList.add(data);
+                        }
+                    }
+                    itemsFiltered = filteredList;
+                }else if (Integer.parseInt(charString)==0)  {
+                    ArrayList<OrdersDataResponse> filteredList = new ArrayList<>();
+                    for (OrdersDataResponse data : items) {
+                        String  status = data.getStatus();
+                        if(status.equalsIgnoreCase("")){
+                            filteredList.add(data);
+                        }
+                    }
+                    itemsFiltered = filteredList;
+                }
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = itemsFiltered;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                itemsFiltered = (ArrayList<OrdersDataResponse>) results.values;
+                notifyDataSetChanged();
+            }
+        };
+
+    }
+
+}
---
+++ app/src/main/java/tech/mlsn/eatgo/response/OrdersDataResponse.java
@@ -0,0 +1,159 @@
+package tech.mlsn.eatgo.response;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by Rzzkan on 30/07/2021.
+ */
+public class OrdersDataResponse {
+    @SerializedName("id_order")
+    @Expose
+    private String idOrder;
+    @SerializedName("id_restaurant")
+    @Expose
+    private String idRestaurant;
+    @SerializedName("id_user")
+    @Expose
+    private String idUser;
+    @SerializedName("menu_name")
+    @Expose
+    private List<String> menuName = null;
+    @SerializedName("menu_price")
+    @Expose
+    private List<String> menuPrice = null;
+    @SerializedName("menu_qty")
+    @Expose
+    private List<String> menuQty = null;
+    @SerializedName("total")
+    @Expose
+    private String total;
+    @SerializedName("point_used")
+    @Expose
+    private String pointUsed;
+    @SerializedName("point_earned")
+    @Expose
+    private String pointEarned;
+    @SerializedName("status")
+    @Expose
+    private String status;
+    @SerializedName("payment")
+    @Expose
+    private String payment;
+    @SerializedName("date")
+    @Expose
+    private String date;
+
+    public OrdersDataResponse(String idOrder, String idRestaurant, String idUser, List<String> menuName, List<String> menuPrice, List<String> menuQty, String total, String pointUsed, String pointEarned, String status, String payment, String date) {
+        this.idOrder = idOrder;
+        this.idRestaurant = idRestaurant;
+        this.idUser = idUser;
+        this.menuName = menuName;
+        this.menuPrice = menuPrice;
+        this.menuQty = menuQty;
+        this.total = total;
+        this.pointUsed = pointUsed;
+        this.pointEarned = pointEarned;
+        this.status = status;
+        this.payment = payment;
+        this.date = date;
+    }
+
+    public String getIdOrder() {
+        return idOrder;
+    }
+
+    public void setIdOrder(String idOrder) {
+        this.idOrder = idOrder;
+    }
+
+    public String getIdRestaurant() {
+        return idRestaurant;
+    }
+
+    public void setIdRestaurant(String idRestaurant) {
+        this.idRestaurant = idRestaurant;
+    }
+
+    public String getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
+
+    public List<String> getMenuName() {
+        return menuName;
+    }
+
+    public void setMenuName(List<String> menuName) {
+        this.menuName = menuName;
+    }
+
+    public List<String> getMenuPrice() {
+        return menuPrice;
+    }
+
+    public void setMenuPrice(List<String> menuPrice) {
+        this.menuPrice = menuPrice;
+    }
+
+    public List<String> getMenuQty() {
+        return menuQty;
+    }
+
+    public void setMenuQty(List<String> menuQty) {
+        this.menuQty = menuQty;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public String getPointUsed() {
+        return pointUsed;
+    }
+
+    public void setPointUsed(String pointUsed) {
+        this.pointUsed = pointUsed;
+    }
+
+    public String getPointEarned() {
+        return pointEarned;
+    }
+
+    public void setPointEarned(String pointEarned) {
+        this.pointEarned = pointEarned;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPayment() {
+        return payment;
+    }
+
+    public void setPayment(String payment) {
+        this.payment = payment;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
