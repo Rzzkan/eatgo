ff58c12a06366730b1b94cde118db7a8b781606b
Aditya Nur Riskan Nugroho <xcyberd@gmail.com>
2021-08-27 09:29:52 +0700

    fixing bugs all order

--- app/src/main/java/tech/mlsn/eatgo/fragment/orders/DetailOrderFragment.java
+++ app/src/main/java/tech/mlsn/eatgo/fragment/orders/DetailOrderFragment.java
@@ -188,7 +188,7 @@ public class DetailOrderFragment extends Fragment {
             public void onResponse(Call<BaseResponse> call, Response<BaseResponse> response) {
                 if (response.body().getSuccess()==1) {
                     snackbar.snackSuccess("Success");
-                    Tools.removeAllFragment(getActivity(),new OrdersFragment(),"orders");
+                    Tools.removeAllFragment(getActivity(),new AllOrdersFragment(),"orders");
                 } else{
                     snackbar.snackError("Failed");
                 }
